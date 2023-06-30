4951bf08e97c8a82f827e7136a2f3d2f39eb3002
Alexandre Lerosier <lerosie221@unicaen.fr>
2023-07-01 00:06:32 +0200

    création class : testJoueur ainsi que ses fonctions

--- jeu_go/src/test/modeles/JoueurTest.java
+++ jeu_go/src/test/modeles/JoueurTest.java
@@ -3,13 +3,44 @@ package modeles;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+/**
+ * 
+ * @author alexl
+ *
+ */
 class JoueurTest {
 
+	/**
+	 * Test du constructeur
+	 */
 	@Test
 	public void testJoueur() {
 		Joueur joueur = new Joueur("test");
-		
 		assertTrue(joueur instanceof Joueur);
+	}
+	
+	/**
+	 * Test de la fonction getCouleur
+	 */
+	@Test
+	public void testGetPrenom() {
+		Joueur joueur = new Joueur("test");
+		assertTrue("test".equals(joueur.getPrenom()));
+		
+		joueur = new Joueur("abc");
+		assertTrue("abc".equals(joueur.getPrenom()));
+	}
+	
+	/**
+	 * Test de la fonction getCouleur, test par le même temps la fonction setCouleur
+	 */
+	@Test
+	public void testGetCouleur() {
+		Joueur joueur = new Joueur("test");
+		assertNull(joueur.getCouleur());
+		
+		joueur.setCouleur(Piece.BLANC);
+		assertEquals(Piece.BLANC, joueur.getCouleur());
 	}
 
 }
